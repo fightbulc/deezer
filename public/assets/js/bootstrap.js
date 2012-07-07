7df53b95222ccfb0f5ca683c697c883b50f1de9d
Maciej Hirsz <maciej.hirsz@gmail.com>
2012-07-07 14:21:50 +0200

    added abstracts

--- public/assets/js/bootstrap.js
+++ public/assets/js/bootstrap.js
@@ -5,7 +5,11 @@ require.config({
     jquery: 'vendor/jquery/jquery-1.7.2',
     text: 'vendor/requirejs/text',
     underscore: 'vendor/backbone/underscore-1.3.3',
-    backbone: 'vendor/backbone/backbone-0.9.2'
+    backbone: 'vendor/backbone/backbone-0.9.2',
+
+    abstractModel: 'js/abstract/abstractModel',
+    abstractCollection: 'js/abstract/abstractCollection',
+    abstractView: 'js/abstract/abstractView'
   },
   shim: {
     backbone: {

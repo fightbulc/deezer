204c33d1336779da38ec41d7d7344a606e65799f
jeremy <jeremy.awon@gmail.com>
2012-07-07 16:27:58 +0200

    added search function to trackSearchCollection

--- public/assets/js/collection/trackSearchCollection.js
+++ public/assets/js/collection/trackSearchCollection.js
@@ -1,8 +1,18 @@
 define(function(require){
 
   var abstractCollection = require('abstractCollection');
+  var Data = require('js/data/data');
 
   var trackSearchCollection = abstractCollection.extend({
+    search: function(query){
+      var collection = this;
+
+      Data.deezerSearchTracks(query).done(function(response){
+        if(_.has(response, 'data')){
+          collection.reset(response['data']);
+        }
+      });
+    }
   });
 
   return trackSearchCollection;

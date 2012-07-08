510b26a086d86210fae3ae938b7440ab40958aa4
Tino Ehrich <ehrich@efides.com>
2012-07-08 16:55:03 +0200

    fixed and dump

--- app/sql/deezer-2012-07-08-latest-local.sql
+++ app/sql/deezer-2012-07-08-latest-local.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.5.25-log)
 # Database: deezer
-# Generation Time: 2012-07-08 14:04:19 +0000
+# Generation Time: 2012-07-08 14:12:22 +0000
 # ************************************************************
 
 
@@ -36,64 +36,6 @@ CREATE TABLE `memories` (
   FULLTEXT KEY `search` (`mood_tag`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-LOCK TABLES `memories` WRITE;
-/*!40000 ALTER TABLE `memories` DISABLE KEYS */;
-
-INSERT INTO `memories` (`id`, `user_id`, `track_id`, `mood_tag`, `created`)
-VALUES
-	(1,1,6461440,'happy',1341665124),
-	(2,3,1109731,'sad',1341665124),
-	(3,1,6026499,'love',1341665124),
-	(4,1,916424,'angry',1341665124),
-	(5,1,916445,'dreamy',1341665124),
-	(6,4,1109730,'dreamy',1341665124),
-	(7,1,2951016,'happy',1341665124),
-	(8,1,6461435,'chilled',1341665124),
-	(9,1,6461430,'thoughtful',1341665124),
-	(10,1,916427,'sleepy',1341665124),
-	(14,2,6026499,'love',1341665124),
-	(15,3,6026499,'hate',1341665124),
-	(16,4,6026499,'rage',1341665124),
-	(17,1,6026499,'exhausted',1341675109),
-	(18,1,6026499,'exhausted',1341675141),
-	(19,1,6026499,'happy',1341675170),
-	(20,1,6026499,'exhausted',1341675195),
-	(21,45403071,6026499,'exhausted',1341696769),
-	(22,45403071,6026499,'sunny',1341739052),
-	(23,45403071,6026499,'sunny',1341744892),
-	(24,45403071,6026499,'sunny',1341744902),
-	(25,45403071,3147243,'growing grass',1341755116),
-	(26,45403071,3147243,'growinggrass',1341755519),
-	(27,45403071,3147243,'growing-grass',1341755578),
-	(28,45403071,13236642,'good-times',1341755945),
-	(29,45403071,2221576,'booyaaa',1341756223);
-
-/*!40000 ALTER TABLE `memories` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# Dump of table story_votes
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `story_votes`;
-
-CREATE TABLE `story_votes` (
-  `story_id` bigint(20) unsigned NOT NULL,
-  `user_id` bigint(20) NOT NULL,
-  `vote` tinyint(1) NOT NULL,
-  KEY `byMemoryId` (`story_id`,`user_id`,`vote`),
-  KEY `byUserId` (`user_id`,`story_id`,`vote`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-LOCK TABLES `story_votes` WRITE;
-/*!40000 ALTER TABLE `story_votes` DISABLE KEYS */;
-
-INSERT INTO `story_votes` (`story_id`, `user_id`, `vote`)
-VALUES
-	(3,45403071,1);
-
-/*!40000 ALTER TABLE `story_votes` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 # Dump of table stories
@@ -111,17 +53,21 @@ CREATE TABLE `stories` (
   KEY `byTrackId` (`track_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `stories` WRITE;
-/*!40000 ALTER TABLE `stories` DISABLE KEYS */;
 
-INSERT INTO `stories` (`id`, `track_id`, `user_id`, `story`, `created`)
-VALUES
-	(1,6026499,45403071,'Foozle.',1341738968),
-	(2,6026499,45403071,'Foozle.',1341739604),
-	(3,6026499,45403071,'Foozle.',1341749535);
 
-/*!40000 ALTER TABLE `stories` ENABLE KEYS */;
-UNLOCK TABLES;
+# Dump of table story_votes
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `story_votes`;
+
+CREATE TABLE `story_votes` (
+  `story_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `vote` tinyint(1) NOT NULL,
+  KEY `byMemoryId` (`story_id`,`user_id`,`vote`),
+  KEY `byUserId` (`user_id`,`story_id`,`vote`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 # Dump of table tracks
@@ -136,17 +82,6 @@ CREATE TABLE `tracks` (
   UNIQUE KEY `byId` (`id`,`artist`,`title`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `tracks` WRITE;
-/*!40000 ALTER TABLE `tracks` DISABLE KEYS */;
-
-INSERT INTO `tracks` (`id`, `artist`, `title`)
-VALUES
-	(2221576,'Big Pun featuring Joe','Still Not A Player'),
-	(3147243,'Radiohead','Creep (Acoustic)'),
-	(13236642,'Plattenpapzt feat. Kool Savas','King Of Rap');
-
-/*!40000 ALTER TABLE `tracks` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 # Dump of table users
